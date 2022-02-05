1958a4df82796c056eca8c1059161acb6787cfb6
Mohd Arsh <70385742+arsh-ash@users.noreply.github.com>
2022-02-05 16:14:03 +0530

    added step field in machine schema

--- models/machine.js
+++ models/machine.js
@@ -1,22 +1,50 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
+
+let MachineSchema = new mongoose.Schema(
+  {
+    machineName: {
+      type: String,
+      required: [true, "Please add Name"],
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    steps: {
+      type: Array,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Machine = mongoose.model("Machine", MachineSchema);
+module.exports = Machine;
+
+// const mongoose =require("mongoose");
  
-let MachineSchema=new mongoose.Schema({
-    machineName:{
-        type:String,
-        required: [true, "Please add Name"],
-    },
-    url:{
-        type:String,
-        required:true
-    },
-    description:{
-        type:String
+// let MachineSchema=new mongoose.Schema({
+//     machineName:{
+//         type:String,
+//         required: [true, "Please add Name"],
+//     },
+//     url:{
+//         type:String,
+//         required:true
+//     },
+//     description:{
+//         type:String
 
-    },
+//     },
     
-},{
- timestamps:true,   
-})
+// },{
+//  timestamps:true,   
+// })
 
-const Machine=mongoose.model("Machine",MachineSchema);
-module.exports=Machine
+// const Machine=mongoose.model("Machine",MachineSchema);
+// module.exports=Machine
