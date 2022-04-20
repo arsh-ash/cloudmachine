7f0e970f8d56774f4b90447932765372f70e1728
arsh-ash <mohdarsh.8826@gmail.com>
2022-04-20 11:56:07 +0530

    commiting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const cors = require('cors');
 
 const app=express();
-const port=8000;
+const port=9000;
 const connectDB=require('./config/database')
 const passport=require("passport");
 const passportJwt=require("./config/passport-jwt-strategy");
